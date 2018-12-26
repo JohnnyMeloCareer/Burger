b83624f1cc0330005e5a86d52f4b4c31308a2461
Johnny Melo <Johnnymelowork@gmail.com>
2018-12-26 14:27:42 -0500

    fixed error on connection.js

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ var connection = mysql.createConnection({
 });
 
 // Make a connection
-HTMLFormControlsCollection.connect(function(err) {
+connection.connect(function(err) {
     if (err) {
         console.error("error connecting: " + err.stack);
         return;
